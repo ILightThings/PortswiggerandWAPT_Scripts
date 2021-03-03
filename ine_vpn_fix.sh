8191bd95497a176b481579319316918913b70894
iLightThings <gitgub@ggray.info>
2021-03-03 14:32:25 -0500

    Create ine_vpn_fix.sh

---
+++ ine_vpn_fix.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! $# == 3 ]; then
+  echo "Usage: vpnfix.sh /path/to/vpn.ovpn username password"
+  exit
+fi
+
+file_path=$(dirname $1)
+echo $file_path
+
+echo $2 > $file_path/cred.txt
+echo $3 >> $file_path/cred.txt
+
+echo "auth-user-pass cred.txt" >> $1
